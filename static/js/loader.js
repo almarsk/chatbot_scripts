d2d2ac5d154c0f941f156d77715f52823c4ff57f
David Lukes <dafydd.lukes@gmail.com>
2020-11-13 17:54:19 +0100

    Schovat prvky při přemýšlení

--- static/js/loader.js
+++ static/js/loader.js
@@ -2,11 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
   var delay = 2500;  // v milisekundách
   var loader = document.getElementById("loader");
   var odpik = document.getElementById("odpik");
+  var chatbot_reply = document.getElementById("chatbot-reply");
   var chat_form = document.getElementById("chat-form");
 
   odpik.addEventListener("click", function(event) {
     event.preventDefault();
     loader.classList.remove("display-none");
+    chatbot_reply.classList.add("display-none");
+    chat_form.classList.add("display-none");
     setTimeout(function() { chat_form.submit() }, delay);
   });
 });
