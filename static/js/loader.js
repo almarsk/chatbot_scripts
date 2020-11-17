599f40092f2b1a4e23cd7433ec3239b4c8917b1c
Albert Marsik <albert.marsik@gmail.com>
2020-11-17 18:19:05 +0100

    poslední úpravy gen1

--- static/js/loader.js
+++ static/js/loader.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
   var loader = document.getElementById("loader");
   var odpik = document.getElementById("odpik");
   var chat_form = document.getElementById("chat-form");
+  var rep = document.getElementById("rep");
 
   odpik.addEventListener("click", function(event) {
     event.preventDefault();
     loader.classList.remove("display-none");
+    rep.classList.add("display-none");
+    chat_form.classList.add("display-none");
+
     setTimeout(function() { chat_form.submit() }, delay);
   });
 });
